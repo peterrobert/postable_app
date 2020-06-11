6f2e79e44b76a463ef2474b02dc303e6629d27d9
peterrobert <pwambui93@gmail.com>
2020-06-11 12:49:51 +0300

    added boostrap and css styling and also layouts

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get 'static_pages/help'
   get 'static_pages/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-root "application#hello"
+root "static_pages#home"
 end
