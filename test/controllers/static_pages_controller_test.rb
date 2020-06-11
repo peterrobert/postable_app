e52b283ef0b28379a2f317ba2cf8f7b9984ec9f1
peterrobert <pwambui93@gmail.com>
2020-06-11 13:50:35 +0300

    added a users controller

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,18 +2,22 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get home_path
     assert_response :success
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
   end
 
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+  end
 end
